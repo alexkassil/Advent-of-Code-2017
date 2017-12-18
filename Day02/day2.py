e9d278098a00235586f2839eea47af2e47a65634
Alex Kassil <alexkassil@sbcglobal.net>
2017-12-17 20:26:15 -0800

    Finish part 2 and add doctests

--- Day02/day2.py
+++ Day02/day2.py
@@ -1,11 +1,40 @@
 def checksum(inpt):
     """Return the sum of the differences between
-    the smallest and largest entry in each row"""
+    the smallest and largest entry in each row
+    >>> checksum([[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]])
+    18
+    """
     result = 0
     for row in inpt:
         result += max(row) - min(row)
     return result
         
+def checksum_divisible(inpt):
+    """Return the sum of the largest quotients in
+    each row that are a whole number
+    >>> checksum_divisible([[5, 9, 2, 8], [9, 4, 7, 3], [3, 8, 6, 5]])
+    9
+    """
+    result = 0
+    for row in inpt:
+        result += max(quotients(row))
+    return result
+
+def quotients(row):
+    """Returns an array of quotients for each entry
+    divided by every other entry if evenly divisible.
+    >>> sorted(quotients([3, 2, 9, 8]))
+    [1, 1, 1, 1, 3, 4]
+    """
+    result = []
+    for entry in row:
+        result.extend(map(lambda x: x//entry, filter(lambda x: x%entry == 0, row)))
+    return result
 
 if __name__ == "__main__":
-    print(checksum([[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]))
+    f = open('input.txt')
+    inpt = f.read().strip()
+    f.close()
+    inpt_lst = [[int(e) for e in t.split('\t')] for t in inpt.split('\n')]
+    print(checksum_divisible(inpt_lst))
+
