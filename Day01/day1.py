1530bcdede933cf4adafd0b7578091509318165d
Alex Kassil <alexkassil@sbcglobal.net>
2017-12-17 19:31:33 -0800

    Created function for part 2

--- Day01/day1.py
+++ Day01/day1.py
@@ -2,7 +2,7 @@ def inverse_captcha(inpt):
     """ Takes in a sequence of digits and returns
     the sum of all digits that match the next digit
     in the list. The list is circular, so digit after 
-    last is first
+    last is first.
     >>> inverse_captcha('1122')
     3
     >>> inverse_captcha('1111')
@@ -15,8 +15,28 @@ def inverse_captcha(inpt):
             result += int(lst[i])
     return result
 
+def inverse_captcha_halfway(inpt):
+    """ Takes in a sequence of digits and returns
+    the sum of all digits that match the digit that is
+    halfawy around the list. The lenght of each list is
+    even, with the first entry matching with the n/2th
+    entry, where n is the end of the list.
+    >>> inverse_captcha_halfway('1212')
+    6
+    >>> inverse_captcha_halfway('1221')
+    0
+    """
+    result = 0
+    lst = list(inpt)
+    n = len(lst)
+    for i in range(len(lst)):
+        if lst[i] == lst[i-(n//2)]:
+            result += int(lst[i])
+    return result
+
 if __name__ == "__main__":
     f = open("input.txt")
-    print(inverse_captcha(f.read().strip()))
+    inpt = f.read().strip()
     f.close()
-    
+    print(inverse_captcha(inpt))    
+    print(inverse_captcha_halfway(inpt))    
