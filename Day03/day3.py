95a1a08d5e919d5258a07558e8f3f42a3d98e7ee
Alex Kassil <alexkassil@sbcglobal.net>
2017-12-18 15:07:37 -0800

    Added functions to complete solution

--- Day03/day3.py
+++ Day03/day3.py
@@ -10,11 +10,24 @@ def find_ring(num):
 def location(num):
     ring = find_ring(num)
     ring_values = list(range((ring - 2) ** 2 + 1, ring ** 2 + 1))
-#    print(ring_values)
     return ring_values
 
 def mid(num):
     ring_values = location(num)
-    mid_val = ring_values[len(location(num))//2-1]
+    mid_val = []
+    length = len(ring_values)
+    for i in range(1, 5):
+        mid_val.append(ring_values[length//4*i-1-length//8])
     return mid_val
 
+def dist_from_mid(num):
+    dist = num**2
+    mid_val = mid(num)
+    for val in mid_val:
+        dist = min(dist, abs(num-val))
+    return dist
+
+def dist(num):
+    if num == 1:
+        return 0
+    return (find_ring(num) - 1)//2 + dist_from_mid(num)
