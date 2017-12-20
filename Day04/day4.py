24b0e07f303e612e8251b824a14d93f5d0e35a1e
Alex Kassil <alexkassil@sbcglobal.net>
2017-12-19 23:51:14 -0800

    Create valid function

--- Day04/day4.py
+++ Day04/day4.py
@@ -1,7 +1,30 @@
-
+def valid(phrase):
+    """ Checks if passphrase only contains distinct
+    words.
+    >>> valid('aa bb cc dd')
+    True
+    >>> valid('aa bb aa')
+    False
+    >>> valid('aa bb aaa')
+    True
+    """
+    words = []
+    series_of_words = phrase.split(' ')
+    words.append(series_of_words.pop())
+    for word in series_of_words:
+        if word in words:
+            return False
+        words.append(word)
+    return True
 
 if __name__ == "__main__":
     f = open("input.txt")
     inpt = f.read().strip()
     f.close()
-    print(inpt)
+    total = 0
+    phrases = inpt.split('\n')
+    print(len(phrases))
+    for phrase in phrases:
+        if valid(phrase):
+            total += 1
+    print(total)
