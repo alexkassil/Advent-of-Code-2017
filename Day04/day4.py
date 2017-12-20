47196f4803b7af8cbce9188c99faad13328b96c7
Alex Kassil <alexkassil@sbcglobal.net>
2017-12-19 23:36:51 -0800

    Test input

--- Day04/day4.py
+++ Day04/day4.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     f = open("input.txt")
     inpt = f.read().strip()
     f.close()
-    print(inverse_captcha(inpt))    
-    print(inverse_captcha_halfway(inpt))    
+    print(inpt)
