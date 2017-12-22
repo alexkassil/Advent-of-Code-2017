3f82277d02dd4625177cc1a32ca2a268c41e2de5
Alex Kassil <alexkassil@sbcglobal.net>
2017-12-21 23:55:29 -0800

    Fix bug to correctly increment cur

--- Day05/day5.py
+++ Day05/day5.py
@@ -2,6 +2,7 @@ def jumps(lst):
     i = 0
     cur = 0
     while cur < len(lst):
-        lst[cur], cur = lst[cur]+1, lst[cur]
+        lst[cur], cur = lst[cur]+1, cur + lst[cur]
         i += 1
     return i
+
